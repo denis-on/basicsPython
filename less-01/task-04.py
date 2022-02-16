ae150c10d3d7b343cb9ef1e2a78b9a5318c5b7ac
denis-on <denis-on@mail.ru>
2022-02-16 17:10:42 +0300

    task-04 ok ---------- task-03 ok task-02 ok task-01 ok

---
+++ less-01/task-04.py
@@ -0,0 +1,25 @@
+# 4. Пользователь вводит целое положительное число.
+# Найдите самую большую цифру в числе.
+# Для решения используйте цикл while и арифметические операции.
+
+while True:
+    try:
+        num = int(input('Введите целое положительное число: '))
+    except ValueError:
+        print('Это не число, попробуйте снова.')
+    else:
+        if num < 0:
+            print('неверно')
+            continue
+        break
+
+num_stat = num
+single = 0
+while num > 0:
+    rem = num % 10
+    print(rem, end=" ")
+    single = rem if rem > single else single
+    num = num // 10
+print('')
+print('%d наибольшая цифра в числе %d' % (single, num_stat) )
+
